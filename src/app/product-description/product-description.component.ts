be7dabb2ee4c1f0734b862c2d6cef1b1c14bdfe2
Bradley Hennen <bradhennen5@gmail.com>
2020-04-29 14:38:30 -0500

    added product service and updated html product info

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "app/product.service";
 
 @Component({
-  selector: 'app-product-description',
-  templateUrl: './product-description.component.html',
-  styleUrls: ['./product-description.component.css']
+  selector: "app-product-description",
+  templateUrl: "./product-description.component.html",
+  styleUrls: ["./product-description.component.css"],
 })
 export class ProductDescriptionComponent implements OnInit {
+  constructor(private _productService: ProductService) {}
 
-  constructor() { }
+  albumInfo;
 
   ngOnInit() {
-  }
+    this._productService
+      .getAlbum(1)
+      .subscribe((response) => {
+        this.albumInfo = response;
+        console.log(response);
 
+      });
+  }
 }
