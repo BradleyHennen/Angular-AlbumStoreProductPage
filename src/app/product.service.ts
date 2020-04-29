47158e4b28285e7422bc16de52ae7add5aa1f717
Bradley Hennen <bradhennen5@gmail.com>
2020-04-29 15:36:02 -0500

    fixed tracklist css and list render

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
-import { map } from "rxjs/add/operator/map";
+// import { map } from "rxjs/add/operator/map";
+import { map } from "rxjs/operators";
+
 import { Observable } from "rxjs/Observable";
 
 import { Album } from "./album";
@@ -14,6 +16,6 @@ export class ProductService {
   getAlbum(id: number): Observable<Album> {
     return this._http
       .get(this._albumUrl)
-      .map((response) => <Album>response.json());
+      .pipe(map((response) => <Album>response.json()));
   }
 }
